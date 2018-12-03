346bc1db3d771a1577af7e5e9ec7966b6bfb0170
jcarli <johannes.carli@gmail.com>
2018-12-03 15:23:03 +0100

    added package

--- client/app/src/main/java/se/gosta/activity/MainActivity.java
+++ client/app/src/main/java/se/gosta/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package se.gosta;
+package se.gosta.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import se.gosta.R;
 
 public class MainActivity extends AppCompatActivity {
 
