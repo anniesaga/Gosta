13416986023b8cc07a887698ea7972d9300b13e9
anniesaga <anniepetersson@live.se>
2018-12-03 19:47:46 +0100

    Fixed server script for CYGWIN with new path

--- server/start_server.sh
+++ server/start_server.sh
@@ -5,5 +5,5 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
    javac -cp server/servlet-api.jar:server/org.json.jar server/WEB-INF/classes/servlets/Companies.java && java -jar server/winstone.jar server/WEB-INF/lib/org.json.jar --webroot server
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
-    javac -cp "server/servlet-api.jar;server/org.json.jar" server/WEB-INF/classes/servlets/Companies.java && java -jar server/winstone.jar server/WEB-INF/lib/org.json.jar --webroot server
+    javac -cp "servlet-api.jar;org.json.jar" WEB-INF/classes/servlets/Companies.java && java -jar winstone.jar WEB-INF/lib/org.json.jar --webroot
 fi
