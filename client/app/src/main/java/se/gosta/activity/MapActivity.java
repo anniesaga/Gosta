c47bd61accf7581dd3e80f4f84a06c7ba64d3712
jcarli <johannes.carli@gmail.com>
2018-12-05 15:57:50 +0100

    Created zoomable and clickable image on mapactivity

--- client/app/src/main/java/se/gosta/activity/MapActivity.java
+++ client/app/src/main/java/se/gosta/activity/MapActivity.java
@@ -1,18 +1,36 @@
 package se.gosta.activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.bottomnavigation.LabelVisibilityMode;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import at.lukle.clickableareasimage.ClickableArea;
+import at.lukle.clickableareasimage.ClickableAreasImage;
+import at.lukle.clickableareasimage.OnClickableAreaClickedListener;
+import at.lukle.clickableareasimage.PixelPosition;
 import se.gosta.R;
-import se.gosta.storage.TouchImageView;
+import se.gosta.storage.Company;
+import se.gosta.storage.Session;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 
-public class MapActivity extends AppCompatActivity{
+public class MapActivity extends AppCompatActivity implements OnClickableAreaClickedListener {
 
-    private final String TAG = MapActivity.class.getSimpleName();
+    private final String LOG_TAG = MapActivity.class.getSimpleName();
+
+    ClickableAreasImage clickableAreasImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +38,17 @@ public class MapActivity extends AppCompatActivity{
         setContentView(R.layout.activity_map);
 
 
-        TouchImageView iv = (TouchImageView) findViewById(R.id.map);
-        iv.setImageResource(R.drawable.gostamap);
+        ImageView img = (ImageView) findViewById(R.id.map);
+        img.setImageResource(R.drawable.gostamap);
+        clickableAreasImage = new ClickableAreasImage(new PhotoViewAttacher(img), this);
+
+        List<ClickableArea> clickableAreas = new ArrayList<>();
+        clickableAreas.add(new ClickableArea(370,473,130,87, Session.get("Acando")));
+        Log.d(LOG_TAG, "Added company: " + Session.get("Acando") + "as clickable");
+        clickableAreasImage.setClickableAreas(clickableAreas);
+
+
+
         BottomNavigationView navigation = (BottomNavigationView)
                 findViewById(R.id.navigation);
         navigation.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_UNLABELED);
@@ -51,42 +78,17 @@ public class MapActivity extends AppCompatActivity{
                         return false;
                     }
                 });
-     /*   iv.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                int X =(int) event.getX();
-                int Y = (int) event.getY();
-                int eventaction = event.getAction();
-                Context context = getApplicationContext();
-                Rect r = new Rect(1030,1371,1109,1492);
-
-                switch(eventaction){
-                    case MotionEvent.ACTION_DOWN:
-                        if(r.contains(X,Y)){
-                            Toast.makeText(context, "Pressed!", Toast.LENGTH_SHORT).show();
-                        }else {
-                            Toast.makeText(context, "ACTION_DOWN AT COORDS " + "X: " + X + "Y: " + Y, Toast.LENGTH_SHORT).show();
-                        }
-                        return true;
-
-                    case MotionEvent.ACTION_MOVE:
-                        Toast.makeText(context, "ACTION_MOVE "+ "X: " + X + "Y: " + Y, Toast.LENGTH_SHORT).show();
-                        return true;
-
-                    case MotionEvent.ACTION_UP:
-                        Toast.makeText(context, "ACTION_UP "+ "X: " + X + "Y: " + Y, Toast.LENGTH_SHORT).show();
-                        return true;
+    }
 
 
-                }
+    @Override
+    public void onClickableAreaTouched(Object item){
+        if (item instanceof Company) {
+        String text = ((Company) item).name();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
 
-                return false;
-
-
-            }
-        });*/
-
-
+        }
+        Log.d(LOG_TAG, "Clicked on ClickableArea");
     }
 
 }
